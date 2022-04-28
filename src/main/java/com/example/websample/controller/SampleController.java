8bf52a4ae53c8ae9b682caa4ab931406ecf0cd19
seyol@kakao.com <yT,DYGB6Bo>
2022-04-28 22:05:36 +0900

    Initial commit

---
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -0,0 +1,52 @@
+package com.example.websample.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class SampleController {
+
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order : " + id);
+        return "orderId:" + id + ", " + "orderAmount:1000";
+    }
+
+    @DeleteMapping("/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("Delete some order : " + id);
+        return "Delete orderId:" + id;
+    }
+
+    @GetMapping("/order")
+    public String getOrderWithRequestParam(
+            @RequestParam(value = "orderId", required = false, defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount) {
+        log.info("Get order : " + id + ", amount : " + amount);
+        return "orderId:" + id + ", " + "orderAmount:" + amount;
+    }
+
+    @PostMapping("/order")
+    public String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId) {
+        log.info("Create order : " + createOrderRequest +
+                ", userAccountId : " + userAccountId);
+        return "orderId:" + createOrderRequest.getOrderId() + ", " +
+                "orderAmount:" + createOrderRequest.getOrderAmount();
+    }
+
+    @PutMapping("/order")
+    public String createOrder() {
+        log.info("Create order");
+        return "order created -> orderId:1, orderAmount:1000";
+    }
+
+    @Data
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
+    }
+}
