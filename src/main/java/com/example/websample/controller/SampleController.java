a576625f5eb50844392f8c1ee9b41ef14cfb95cd
seyol@kakao.com <yT,DYGB6Bo>
2022-05-04 01:03:26 +0900

    feat: 강의 촬영 중 exception handler 추가 완료

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -1,19 +1,42 @@
 package com.example.websample.controller;
 
+import com.example.websample.dto.ErrorResponse;
+import com.example.websample.exception.ErrorCode;
+import com.example.websample.exception.WebSampleException;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @Slf4j
 @RestController
 public class SampleController {
 
     @GetMapping("/order/{orderId}")
-    public String getOrder(@PathVariable("orderId") String id) throws IllegalAccessException {
+    public String getOrder(@PathVariable("orderId") String id) throws IllegalAccessException, SQLIntegrityConstraintViolationException {
         log.info("Get some order : " + id);
 
         if ("500".equals(id)) {
-            throw new IllegalAccessException("500 is not valid orderId.");
+            throw new WebSampleException(
+                    ErrorCode.TOO_BIG_ID_ERROR,
+                    "500 is too big orderId."
+            );
+        }
+
+        if ("3".equals(id)) {
+            throw new WebSampleException(
+                    ErrorCode.TOO_SMALL_ID_ERROR,
+                    "3 is too small orderId."
+            );
+        }
+
+        if ("4".equals(id)) {
+            throw new SQLIntegrityConstraintViolationException(
+                    "Duplicated insertion was tried."
+            );
         }
         return "orderId:" + id + ", " + "orderAmount:1000";
     }
